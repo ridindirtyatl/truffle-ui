43b349b17bd22a04d04068b2da270c1152adf462
Taggart Bowen-Gaddy <taggartbg@gmail.com>
2017-02-25 18:24:43 -0500

    HACKS!

--- map.js
+++ map.js
@@ -28,14 +28,33 @@ MARTAMap.prototype.updateHeatmap = function(busCollection) {
 
   busCollection.forEach(function(bus) {
     heatmapData.push({
-      location: bus[0] && bus[1] && new google.maps.LatLng(bus[0], bus.[0]),
-      weight: bus[2] || 1
+      location: new google.maps.LatLng(bus[0][0], bus[0][1]),
+      weight: bus[1] * 100 || 1
     });
   });
 
   this.heatmap = new google.maps.visualization.HeatmapLayer({
-    data: heatmapData
+    data: heatmapData,
+    radius: 20,
+    maxIntensity: 10000
   });
+
+// HACKKK
+  var opts = {
+    panControl: false,
+    mapTypeControl: false,
+    streetViewControl: false,
+    zoomControlOptions: {
+        position: google.maps.ControlPosition.LEFT_CENTER
+    },
+    center: this.mapCenter,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  this.mapObj = new google.maps.Map(this.mapEl, opts);
+// end hack
+
   this.heatmap.setMap(this.mapObj);
 }
 
